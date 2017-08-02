e22e520af1fac45401e959ef415fb8a4cc6be251
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-08-02 13:05:39 +0530

    Fix timezone handling for meeting booking
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- static/js/scheduling_ui.js
+++ static/js/scheduling_ui.js
@@ -5,7 +5,10 @@ window.addEventListener('load', function () {
   flatpickr('.start_date_input', {
     enableTime: true,
     defaultDate: new Date(Date.now() + 300000),
-    minDate: new Date(Date.now() + 60000)
+    minDate: new Date(Date.now() + 60000),
+    altInput: true,
+    altFormat: 'h:i K, M j, Y',
+    dateFormat: 'Z'
   });
 
 });
